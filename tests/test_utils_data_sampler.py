2215a1e3f40a9d9c74ab6554784b7af57af75d40
Labbeti <labbeti.pub@gmail.com>
2025-12-21 03:08:17 +0100

    fix: update Counter check.

--- tests/test_utils_data_sampler.py
+++ tests/test_utils_data_sampler.py
@@ -8,7 +8,7 @@ from unittest import TestCase
 from torchwrench.utils.data.sampler import BalancedSampler
 
 
-class TestCollate(TestCase):
+class TestSampler(TestCase):
     def test_balanced_sampler(self) -> None:
         indices_per_class = [[0, 1], [2], [3, 4, 5]]
 
@@ -30,6 +30,8 @@ class TestCollate(TestCase):
             result_counter = Counter(result_cls_indices)
             expected_counter = Counter(
                 {i: n_max_iterations // num_classes for i in range(num_classes)}
+                if num_sampled_per_cls > 0
+                else {}
             )
 
             for i in range(0, len(result_cls_indices), num_classes):
